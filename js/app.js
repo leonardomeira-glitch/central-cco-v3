6ac3054036f25b9e6a407ffcf7bbdcfb907a4607
leonardomeira-glitch <leonardo.meira@triviatrens.com.br>
2026-07-13 15:56:02 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    console.log("Central CCO V3 iniciada.");
 
+    const { data, error } = await window.supabaseClient.auth.getSession();
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log("Sessão:", data.session);
+});
