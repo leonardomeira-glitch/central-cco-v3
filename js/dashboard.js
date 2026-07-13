c8875ac92e45998fead0752ff4ea67c57b36d69b
leonardomeira-glitch <leonardo.meira@triviatrens.com.br>
2026-07-13 17:05:21 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,47 @@
+window.dashboard = {
 
+    iniciar() {
+
+        const app = document.getElementById("app");
+
+        app.innerHTML = `
+
+        <header class="topo">
+
+            <h2>Central de Acessos CCO</h2>
+
+            <div>
+
+                <strong>${window.usuarioAtual.nome}</strong><br>
+
+                ${window.usuarioAtual.perfil}
+
+            </div>
+
+        </header>
+
+        <main id="conteudo">
+
+            <h3>Bem-vindo</h3>
+
+            <p>Sistema iniciado com sucesso.</p>
+
+        </main>
+
+        <footer>
+
+            <button id="btnSairSistema">
+                Sair
+            </button>
+
+        </footer>
+
+        `;
+
+        document
+            .getElementById("btnSairSistema")
+            .onclick = logout;
+
+    }
+
+};
