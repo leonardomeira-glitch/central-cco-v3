a0d484e96fd5628249a7e4a02e49439d4b042b4b
leonardomeira-glitch <leonardo.meira@triviatrens.com.br>
2026-07-13 15:52:42 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://quytcvcwwuwibqzmrjtj.supabase.co/rest/v1/";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_7Y-WGRM9fmcVnMwIp1dq6A_HJ4LHJzk";
 
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
