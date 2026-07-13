0f014467bdbf579ff17db7df39bedb2f182fe1ae
leonardomeira-glitch <leonardo.meira@triviatrens.com.br>
2026-07-13 16:39:35 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,26 @@
+async function login(email, senha) {
+  const { data, error } = await window.supabaseClient.auth.signInWithPassword({
+    email,
+    password: senha
+  });
 
+  if (error) {
+    alert(error.message);
+    return null;
+  }
+
+  return data.user;
+}
+
+async function logout() {
+  await window.supabaseClient.auth.signOut();
+  location.reload();
+}
+
+async function usuarioLogado() {
+  const {
+    data: { user }
+  } = await window.supabaseClient.auth.getUser();
+
+  return user;
+}
